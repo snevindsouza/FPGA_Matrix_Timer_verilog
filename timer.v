875135ab02e6ff9b12c0d88001347493603d10b6
Snevin Leoneel Dsouza <81520536+snevindsouza@users.noreply.github.com>
2023-11-02 14:17:01 +0530

    updated with buzzer functionality

--- timer.v
+++ timer.v
@@ -1,15 +1,16 @@
 `timescale 1ns / 1ps
 module matrix (
 	input clk, enable,
-    	input [3:0] in,         //dip switch select lines
-    	input [3:0] row,       
-    	output reg [3:0] col,
-    	output reg [3:0] ctrl,
-    	output reg [7:0] segment
+	input [3:0] in,         //dip switch select lines
+   	input [3:0] row,
+	output reg buzzer,
+   	output reg [3:0] col,
+   	output reg [3:0] ctrl,
+   	output reg [7:0] segment
 );
 
 integer i, j;       //temporary values 
-reg clkdiv, clkdiv2; 
+reg clkdiv, clkdiv2, count; 
 reg [2:0] flag; 
 reg [3:0] first;    //digits (units)
 reg [3:0] second;   //(tens)
@@ -20,7 +21,8 @@ reg [3:0] val;
 initial begin
 	i = 0;
 	j = 0;
-        col = 4'b0001;
+	count = 1'b0;
+	col = 4'b0001;
 	first = 4'b0000;
 	second = 4'b0000;
 	third = 4'b000;
@@ -50,7 +52,7 @@ always @(posedge clk) begin
         	else if (row[2]==1) val = 4'h8;
         	else if (row[3]==1) val = 4'hc;
     	end
-    	else if (col==4'b0010) begin
+		else if (col==4'b0010) begin
         	if (row[0]==1) val = 4'h1;
         	else if (row[1]==1) val = 4'h5;
         	else if (row[2]==1) val = 4'h9;
@@ -73,45 +75,61 @@ end
 //do not enter values greater than 9. 
 //Max value = 99:59 (MM:SS)
 	
-//Algorithm for BCD down counting
+//Algorithm for Timer down counting
 always @(posedge clkdiv) begin  //1Hz clock (increased frequency)
-	// Assign values to the digits (registers)
-	if (in==4'b0001) first = val;
-    	else if (in==4'b0010) second = val;
-    	else if (in==4'b0100) third = val;
-    	else if (in==4'b1000) fourth = val;
-    	else begin
-        	first = first;
-        	second = second;
-        	third = third;
-        	fourth = fourth;
-    	end
+	// Assign values to the digits (registers) from the 4X4 matrix keypad
+	if (~enable) begin
+		if (in==4'b0001) first = val;
+    		else if (in==4'b0010) second = val;
+    		else if (in==4'b0100) third = val;
+    		else if (in==4'b1000) fourth = val;
+    		else begin
+        		first = first;
+        		second = second;
+        		third = third;
+        		fourth = fourth;
+			buzzer = 1'b0;
+			count = 1'b1;
+    		end
+	end
 	// Start the timer when enabled 
-	if (enable) begin
-        	first = first-1;
-        	if (first == 4'd15) begin 
-	    		first = 4'd9;
-            		second = second-1; 
-        	end
-        	if (second == 4'd15) begin
-            		second = 4'd5;
-            		third = third-1;
-        	end
-        	if (third == 4'd15) begin
-            		third = 4'd9;
-            		fourth = fourth-1;
-        	end
-		if (fourth == 4'd15) begin
-            		fourth = 4'd9;
-        	end
-    	end
-	//Pause the timer when not enabled 
-    	else begin
-        	first = first;
-        	second = second;
-        	third = third;
-        	fourth = fourth;
-    	end
+	else if (enable) begin
+		if (count) begin
+        		first = first-1;
+        		if (first == 4'd15) begin 
+	    			first = 4'd9;
+            			second = second-1; 
+        		end
+        		if (second == 4'd15) begin
+            			second = 4'd5;
+            			third = third-1;
+        		end
+        		if (third == 4'd15) begin
+            			third = 4'd9;
+            			fourth = fourth-1;
+        		end
+			if (fourth == 4'd15) begin
+            			fourth = 4'd9;
+        		end
+			//Stop the timer at 0000 and ring the buzzer
+			else if ((first == 4'd0)&&(second == 4'd0)&&(third == 4'd0)&&(fourth == 4'd0)) begin
+				count = ~count;
+				first = first;
+				second = second;
+				third = third;
+				fourth = fourth;
+				buzzer = 1'b1;
+			end
+		end
+	end
+	//pause the timer 
+	else begin
+		first = first;
+		second = second;
+		third = third;
+		fourth = fourth;
+	end
+	
 end
 
 //Display values in multiplexed 7-degment displays
@@ -132,7 +150,7 @@ always@(posedge clkdiv2) begin
 		endcase
 		flag = 2'b01;
 	end
-	else if (flag == 2'b01) begin
+		else if (flag == 2'b01) begin
 		ctrl = 4'b1101;
 		case (second)
 			4'd0:segment=8'b11111100;
